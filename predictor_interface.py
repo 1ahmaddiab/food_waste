3986391454e71b767c3bdc1e3cfbd4a6ed8817af
Fernanda Caldeira Takahashi <FeTaka@users.noreply.github.com>
2021-11-27 16:20:42 +0100

    Update predictor_interface.py

--- predictor_interface.py
+++ predictor_interface.py
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-import streamlit as st
-from PIL import Image
-from freshness_predictor import predict
-st.set_page_config(page_title='Food Waste Challenge', page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
-st.title("Reducing Food Waste")
-
-row1_1, row1_2 = st.columns((2, 3))
-
-with row1_1:
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image.resize((200, 200)), caption='Uploaded Image.', use_column_width=True)
-
-with row1_2:
-    if uploaded_file is not None:
-        st.write("")
-        st.write("Classifying...")
-        label = predict(image)
-        st.write("***It looks {}!***".format(label))
-=======
 import streamlit as st
 from PIL import Image
 from freshness_predictor import predict
@@ -46,4 +24,3 @@ with row1_2:
             consumers_list = get_consumers_for_label(prediction)
             for i in consumers_list:
                 st.success("{}".format(i))
->>>>>>> Stashed changes
