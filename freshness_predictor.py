0e2087c5e72a19cc0823e2af1dcbe90d3d00a4ab
Fernanda Caldeira Takahashi <FeTaka@users.noreply.github.com>
2021-11-27 16:20:08 +0100

    Update freshness_predictor.py

--- freshness_predictor.py
+++ freshness_predictor.py
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-
-from keras.preprocessing.image import img_to_array
-import numpy as np
-from keras.models import load_model
-
-fruit_names = ["Pretty", "Pretty ugly"]
-
-
-def predict(image1):
-    image = image1.resize((100, 100))
-    image = img_to_array(image)
-    image = image / 255.0
-
-    model = load_model('second_try_food_waste_model.h5')
-
-    prediction_image = np.expand_dims(image, axis=0)
-    prediction = model.predict(prediction_image)
-    value = np.argmax(prediction)
-    return fruit_names[value]
-=======
-
 from keras.preprocessing.image import img_to_array
 import numpy as np
 from keras.models import load_model
@@ -35,4 +13,3 @@ def predict(image1):
     prediction = model.predict(prediction_image)
     value = np.argmax(prediction)
     return value
->>>>>>> Stashed changes
